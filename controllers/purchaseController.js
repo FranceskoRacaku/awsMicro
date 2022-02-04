3a66cc175e6e93df28e2e2dd35d2af4495e1aee8
Francesko Racaku <franceskoracaku@hotmail.com>
2022-02-04 10:42:32 -0500

    testing

--- controllers/purchaseController.js
+++ controllers/purchaseController.js
@@ -28,7 +28,6 @@ const getAllPurchases = async (req, res) => {
 
     // using the builtin 'findOne' function on User Model
     let purchases = await Purchase.findAll({
-        where: {userId: userId},
         attributes:{exclude:['updatedAt','createdAt']},
     })
     res.status(200).send(purchases)
