3405a92eaed33a5046575cc54f4e291038380e8c
Francesko Racaku <franceskoracaku@hotmail.com>
2022-02-07 11:49:43 -0500

    sfjsf

--- models/index.js
+++ models/index.js
@@ -59,7 +59,7 @@ db.Purchases = require('./purchasesModel')(sequelize, DataTypes)
 
 // sync the db by running the model
 // force: false ensure that the table is not created again on every time the program runs
-db.sequelize.sync({ force: true }).then(() => {
+db.sequelize.sync({ force: false }).then(() => {
     console.log('DB synced with sequelize')
 }).catch((error) => {
     console.log('Error syncing the DB to sequelize' + error)
